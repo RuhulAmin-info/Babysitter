edd5a801765c6adadf6a14b1dd1e81d274de22bf
Ruhul Amin <ruhulinfo50@gmail.com>
2020-03-06 22:38:21 +0600

    update index.php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 
 <head>
-	
+
 	<title>Bayasitter-Service</title>
 	<link rel="icon" href="imgs/favicon.png">
 	<link rel="stylesheet" href="css/all.min.css">
 	<link rel="stylesheet" href="css/fontawesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-	
+
 </head>
+
 <body>
 	<nav id="navber">
 		<h1 class="logo">
@@ -30,13 +31,13 @@
 		<div class="full">
 			<div class="header-text">
 
-				<h1>Wellcome <span >To</span>
+				<h1>Wellcome <span>To</span>
 				</h1>
 				<h2 class="m-headding">Babysitting Service Center</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic modi ea animi odio, voluptatibus officiis repellat eius id ipsa sapiente suscipit eos architecto nesciunt culpa ducimus nihil doloremque vitae pariatur?</p>
-				
+
 			</div>
-				<a href="" class="start-btn">Join Now</a>
+			<a href="" class="start-btn">Join Now</a>
 		</div>
 	</div>
 
@@ -58,7 +59,7 @@
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro qui consectetur cumque. Fugiat molestiae facere in fugit aliquid sapiente magni, ea minus quos minima vero dolorem, pariatur, inventore explicabo nostrum.</p>
 		</div>
 		<div class="what-img">
-			
+
 
 			<img src="imgs/value.jpg" alt="">
 		</div>
@@ -79,84 +80,85 @@
 
 
 	<section id="who">
-		
+
 		<div class="wrapper">
-        	<h2>We Are</h2>
+			<h2>We Are</h2>
 
-		        <div class="our_team">
-		            
-		            <div class="team_member">
-		                <div class="team_img">
-		                    <img src="imgs/ruhul.png" alt="">
-		                </div>
-		                <h3>Ruhul Amin</h3>
-		                <p class="role">Developer</p>
-		                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
-		                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
+			<div class="our_team">
 
-		                </p>
-		            </div>
-		            <div class="team_member">
-		                    <div class="team_img">
-		                        <img src="imgs/ruhul.png" alt="">
-		                    </div>
-		                    <h3>Ruhul Amin</h3>
-		                    <p class="role">Developer</p>
-		                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
-		                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
-		                    </p>
-		            </div>
-		            <div class="team_member">
-			            <div class="team_img">
-			                    <img src="imgs/ruhul.png" alt="">
-			            </div>
-		            	<h3>Ruhul Amin</h3>
-		                    <p class="role">Developer</p>
-		                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
-		                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
-		                    </p>  
-		            </div>
-		           
-		            
+				<div class="team_member">
+					<div class="team_img">
+						<img src="imgs/ruhul.png" alt="">
+					</div>
+					<h3>Ruhul Amin</h3>
+					<p class="role">Developer</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
 
-		        </div>
-       
-    	</div>
+					</p>
+				</div>
+				<div class="team_member">
+					<div class="team_img">
+						<img src="imgs/ruhul.png" alt="">
+					</div>
+					<h3>Ruhul Amin</h3>
+					<p class="role">Developer</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
+					</p>
+				</div>
+				<div class="team_member">
+					<div class="team_img">
+						<img src="imgs/ruhul.png" alt="">
+					</div>
+					<h3>Ruhul Amin</h3>
+					<p class="role">Developer</p>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia! Velit reiciendis odio veritatis non.
+					</p>
+				</div>
 
-	</section >
+
+
+			</div>
+
+		</div>
+
+	</section>
 	<div class="footer">
 		<div class="f-1 contact">
-				<h2>Company & Resources</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed pariatur nisi, vel. Minima, culpa tempore libero aspernatur consequatur ratione distinctio! Asperiores dolores, tempora neque, aperiam explicabo quidem temporibus voluptas, sunt velit, non pariatur et? Ut.</p>
+			<h2>Company & Resources</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed pariatur nisi, vel. Minima, culpa tempore libero aspernatur consequatur ratione distinctio! Asperiores dolores, tempora neque, aperiam explicabo quidem temporibus voluptas, sunt velit, non pariatur et? Ut.</p>
 		</div>
 		<div class="f-1 contact">
 			<h2>Mission & Vission</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed pariatur nisi, vel. Minima, culpa tempore libero aspernatur consequatur ratione distinctio! Asperiores dolores, tempora neque, aperiam explicabo quidem temporibus voluptas, sunt velit, non pariatur et? Ut.</p>
-		</div >
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed pariatur nisi, vel. Minima, culpa tempore libero aspernatur consequatur ratione distinctio! Asperiores dolores, tempora neque, aperiam explicabo quidem temporibus voluptas, sunt velit, non pariatur et? Ut.</p>
+		</div>
 		<div class="contact f-1 ">
 			<h2>What's your Mind?</h2>
-			 <form action="" method="post"> 
-            <div class="form-group">
-	            <label for="name">Name</label>
-	            <input type="text" name="name" value=""required>
-            </div>
-             <div class="form-group">
-	            <label for="email">Email</label>
-	            <input type="email" name="email" value="" required>
-            </div>
-            <div class="form-group">
-	            <label for="message">Message</label>
-	            <textarea name="massage" id="" rows="7"></textarea>
-            </div>
-           
-          <button type="submit" class="btn" name="submit">Send Message</button>
-        </form>
-      </div>
+			<form action="" method="post">
+				<div class="form-group">
+					<label for="name">Name</label>
+					<input type="text" name="name" value="" required>
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" name="email" value="" required>
+				</div>
+				<div class="form-group">
+					<label for="message">Message</label>
+					<textarea name="massage" id="" rows="7"></textarea>
+				</div>
+
+				<button type="submit" class="btn" name="submit">Send Message</button>
+			</form>
 		</div>
+	</div>
 	</div>
 	<div class="copy-right">
 		<p>©ruhul2020, all rights reserved</p>
 	</div>
 
 </body>
+
 </html>
